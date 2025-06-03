031c350f334737b28c40fe8b3581d27dd45e0b6b
josephsbtn <josephsebastian8107@gmail.com>
2025-06-03 15:32:16 +0700

    fixing server.js process

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const process = require("process");
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.STATUS === "production" ? "localhost" : "0.0.0.0",
+    host: process.env.STATUS !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
